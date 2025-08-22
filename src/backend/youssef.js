95dc090c0c650c57149c1959876a4b5702150c89
Youssef <ym849447@gmail.com>
2025-08-22 20:37:51 +0300

    Version 2

--- src/backend/youssef.js
+++ src/backend/youssef.js
@@ -6,7 +6,7 @@ export const youssef = {
     job: "Frontend Developer",
     bio: "A creative frontend developer who transforms concepts into fast, elegant, and mobile-friendly web applications using React, JavaScript, HTML, and CSS. Always exploring the next big thing in web development.",
     skills: {
-        frontend: [{ name: "HTML", exp: 95 }, { name: "CSS", exp: 75 }, { name: "JavaScript", exp: 85 }, { name: "React", exp: 90 }, { name: "Bootstrap", exp: 90 }],
+        frontend: [{ name: "HTML", exp: 95 }, { name: "CSS", exp: 75 }, { name: "JavaScript", exp: 85 }, { name: "React", exp: 90 }, { name: "Next.js", exp: 95 }],
         tools: [{ name: "Git", exp: 90 }, { name: "GitHub", exp: 90 }, { name: "Postman", exp: 90 }],
     },
     languages: ["Arabic (Native)", "English (Professional)"],
@@ -64,6 +64,14 @@ export const youssef = {
             tech: ["HTML", "CSS", "React", "Bootstrap"],
             liveLink: "https://todo-list-using-react-smoky.vercel.app/",
             githubLink: "https://github.com/You-s-sef24/TodoList-Using-React"
+        }, {
+            id: 6,
+            name: "Cookpedia",
+            thumbnail: './images/projects/Cookpedia.png',
+            description: "A Next.js app using TheMealDB API with features like browsing meals by category or region, viewing featured recipes, and a responsive design for smooth navigation.",
+            tech: ["HTML", "CSS", "React", "Bootstrap", "Next.js"],
+            liveLink: "https://cookpedia-eta.vercel.app/",
+            githubLink: "https://github.com/You-s-sef24/cookpedia"
         }
     ],
     contactForm: {
